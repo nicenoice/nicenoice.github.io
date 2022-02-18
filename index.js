3e26135b5717aa37b798fb46240115295d382868
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2022-02-18 10:05:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Unfinished
+function newEl (a, b) {
+  const el = document.createElement(a);
+  b.appendChild(el);
+}
+
+newEl('head', document.querySelector('html'));
+var metaEl = document.createElement('meta');
+metaEl.charset = 'utf-8';
+document.title = 'Nice Noice Studio';
+document.querySelector('head').appendChild(metaEl);
+newEl('body', document.querySelector('html'));
